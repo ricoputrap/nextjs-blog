d68012e367339b58beb09a9ec0b38c5bbded37b9
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-07-20 16:08:40 +0700

    Implement basic navigation Link component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import Link from 'next/link'
 
 const FirstPost = () => {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
 }
 
 export default FirstPost
