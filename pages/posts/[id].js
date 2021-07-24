fcfefdc4420d4c3d985db8ebdf0c2a808d9bb04b
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-07-24 14:00:06 +0700

    Polish the date format using date-fns date formatting library

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,20 @@
+import Head from 'next/head';
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 const Post = ({ postData }) => {
   return (
     <Layout>
+      <Head>
+        <title>{ postData.title }</title>
+      </Head>
+
       { postData.title }
       <br />
       { postData.id }
       <br />
-      { postData.date }
+      <Date dateString={ postData.date } />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
